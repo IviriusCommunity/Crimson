95a0544c094a0713c787090bbff384024d8db373
Lamparter <notlamparter@outlook.com>
2024-10-12 18:30:56 +0100

    Add legacy controls from `Ivirius.UI.Controls`
    Co-authored-by: ErrorTek <100033309+errortek@users.noreply.github.com>
    Co-authored-by: IviriusMain <106150547+IviriusMain@users.noreply.github.com>

---
+++ Toolkit/Controls/Card.cs
@@ -0,0 +1,112 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Crimson.Toolkit.Controls
+{
+    public sealed class Card : Control
+    {
+        public Card()
+        {
+            this.DefaultStyleKey = typeof(Card);
+            this.PointerEntered += Card_PointerEntered;
+            this.PointerPressed += Card_PointerPressed;
+            this.PointerReleased += Card_PointerReleased;
+            this.PointerExited += Card_PointerExited;
+        }
+
+        public static readonly DependencyProperty TitleProperty =
+        DependencyProperty.Register(
+        "Title", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Title") // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The title of the Card")]
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty SubtitleProperty =
+        DependencyProperty.Register(
+        "Subtitle", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Subtitle") // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The Subtitle of the Card")]
+        public string Subtitle
+        {
+            get { return (string)GetValue(SubtitleProperty); }
+            set { SetValue(SubtitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty ContentProperty =
+        DependencyProperty.Register(
+        "Content", // The name of the property
+        typeof(string), // The type of the property
+        typeof(Card), // The type of the owner class
+        new PropertyMetadata("Content") // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The Content of the Card")]
+        public string Content
+        {
+            get { return (string)GetValue(ContentProperty); }
+            set { SetValue(ContentProperty, value); }
+        }
+
+        private bool invokedFromLeftButton;
+
+        private void Card_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "Normal", true);
+        }
+
+        private void Card_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            VisualStateManager.GoToState(this, "PointerOver", true);
+            if (invokedFromLeftButton == true) this.Click?.Invoke(this, new RoutedEventArgs());
+        }
+
+        private void Card_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed == true)
+            {
+                VisualStateManager.GoToState(this, "Pressed", true);
+                invokedFromLeftButton = true;
+            }
+            else invokedFromLeftButton = false;
+        }
+
+        private void Card_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed == false) VisualStateManager.GoToState(this, "PointerOver", true);
+            if (e.GetCurrentPoint(this).Properties.IsLeftButtonPressed == true) VisualStateManager.GoToState(this, "Pressed", true);
+        }
+
+        public event RoutedEventHandler Click;
+    }
+}
---
+++ Toolkit/Controls/ChatBubble.cs
@@ -0,0 +1,48 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Documents;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Crimson.Toolkit.Controls
+{
+    public sealed class ChatBubble : Control
+    {
+        public ChatBubble()
+        {
+            this.DefaultStyleKey = typeof(ChatBubble);
+        }
+
+        public static readonly DependencyProperty TextProperty =
+        DependencyProperty.Register(
+        "Text", // The name of the property
+        typeof(string), // The type of the property
+        typeof(ChatBubble), // The type of the owner class
+        new PropertyMetadata("Text") // Default value
+        );
+
+        [Browsable(true)]
+        [Category("Common")]
+        [Description("The text in the ChatBubble")]
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+    }
+}
